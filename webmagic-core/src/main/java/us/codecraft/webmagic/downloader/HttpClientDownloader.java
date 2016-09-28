bb7c87f387f8313dfec5bfffdb88682633ad6b3b
weirongzhi <weirongzhi@ufenqi.com>
2016-09-28 16:08:33 +0800

    对Request增加了一个字段 boolean isNeedCycleCraw,在scheduler里面判断是否需要重复，因为如果模拟登录，但是也使用FileCacheQueueScheduler时，登录成功前的URL只会抓取一次，作者后来在scheduler中增加了一个setRegex(regex)来判断是否抓取，我感觉这适合抓取一批URL。另外，对于scheduler，我将setRegex(String regex)变成了setRegexs(String...regexs)这样可以设置多个正则表达式。 	此外，对于downloder的处理，在Page download(Reuqest,task)方法中，对于返回的statusCode的处理比较简单，我有增加了对常见的几种状态码的判断，让除了200状态码之外的其他状态码的提示更具体。这一点我感觉是很有必要的，比如模拟登录学信网，登录成功后返回的状态码是302，如果按照之前的downloder就不会往前走。

--- webmagic-core/src/main/java/us/codecraft/webmagic/downloader/HttpClientDownloader.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/downloader/HttpClientDownloader.java
@@ -1,10 +1,12 @@
 package us.codecraft.webmagic.downloader;
 
 import com.google.common.collect.Sets;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.annotation.ThreadSafe;
 import org.apache.http.client.config.CookieSpecs;
@@ -20,6 +22,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
@@ -35,218 +38,267 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
 /**
  * The http downloader based on HttpClient.
- *
+ * 
  * @author code4crafter@gmail.com <br>
  * @since 0.1.0
  */
 @ThreadSafe
 public class HttpClientDownloader extends AbstractDownloader {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private final Map<String, CloseableHttpClient> httpClients = new HashMap<String, CloseableHttpClient>();
+	private final Map<String, CloseableHttpClient> httpClients = new HashMap<String, CloseableHttpClient>();
 
-    private HttpClientGenerator httpClientGenerator = new HttpClientGenerator();
+	private HttpClientGenerator httpClientGenerator = new HttpClientGenerator();
 
-    private CloseableHttpClient getHttpClient(Site site, Proxy proxy) {
-        if (site == null) {
-            return httpClientGenerator.getClient(null, proxy);
-        }
-        String domain = site.getDomain();
-        CloseableHttpClient httpClient = httpClients.get(domain);
-        if (httpClient == null) {
-            synchronized (this) {
-                httpClient = httpClients.get(domain);
-                if (httpClient == null) {
-                    httpClient = httpClientGenerator.getClient(site, proxy);
-                    httpClients.put(domain, httpClient);
-                }
-            }
-        }
-        return httpClient;
-    }
+	private CloseableHttpClient getHttpClient(Site site, Proxy proxy) {
+		if (site == null) {
+			return httpClientGenerator.getClient(null, proxy);
+		}
+		String domain = site.getDomain();
+		CloseableHttpClient httpClient = httpClients.get(domain);
+		if (httpClient == null) {
+			synchronized (this) {
+				httpClient = httpClients.get(domain);
+				if (httpClient == null) {
+					httpClient = httpClientGenerator.getClient(site, proxy);
+					httpClients.put(domain, httpClient);
+				}
+			}
+		}
+		return httpClient;
+	}
 
-    @Override
-    public Page download(Request request, Task task) {
-        Site site = null;
-        if (task != null) {
-            site = task.getSite();
-        }
-        Set<Integer> acceptStatCode;
-        String charset = null;
-        Map<String, String> headers = null;
-        if (site != null) {
-            acceptStatCode = site.getAcceptStatCode();
-            charset = site.getCharset();
-            headers = site.getHeaders();
-        } else {
-            acceptStatCode = Sets.newHashSet(200);
-        }
-        logger.info("downloading page {}", request.getUrl());
-        CloseableHttpResponse httpResponse = null;
-        int statusCode=0;
-        try {
-            HttpHost proxyHost = null;
-            Proxy proxy = null; //TODO
-            if (site.getHttpProxyPool() != null && site.getHttpProxyPool().isEnable()) {
-                proxy = site.getHttpProxyFromPool();
-                proxyHost = proxy.getHttpHost();
-            } else if(site.getHttpProxy()!= null){
-                proxyHost = site.getHttpProxy();
-            }
-            
-            HttpUriRequest httpUriRequest = getHttpUriRequest(request, site, headers, proxyHost);//���������˴���
-            httpResponse = getHttpClient(site, proxy).execute(httpUriRequest);//getHttpClient�������˴�����֤
-            statusCode = httpResponse.getStatusLine().getStatusCode();
-            request.putExtra(Request.STATUS_CODE, statusCode);
-            if (statusAccept(acceptStatCode, statusCode)) {
-                Page page = handleResponse(request, charset, httpResponse, task);
-                onSuccess(request);
-                return page;
-            } else {
-                logger.warn("code error " + statusCode + "\t" + request.getUrl());
-                return null;
-            }
-        } catch (IOException e) {
-            logger.warn("download page " + request.getUrl() + " error", e);
-            if (site.getCycleRetryTimes() > 0) {
-                return addToCycleRetry(request, site);
-            }
-            onError(request);
-            return null;
-        } finally {
-        	request.putExtra(Request.STATUS_CODE, statusCode);
-            if (site.getHttpProxyPool()!=null && site.getHttpProxyPool().isEnable()) {
-                site.returnHttpProxyToPool((HttpHost) request.getExtra(Request.PROXY), (Integer) request
-                        .getExtra(Request.STATUS_CODE));
-            }
-            try {
-                if (httpResponse != null) {
-                    //ensure the connection is released back to pool
-                    EntityUtils.consume(httpResponse.getEntity());
-                }
-            } catch (IOException e) {
-                logger.warn("close response fail", e);
-            }
-        }
-    }
+	@Override
+	public Page download(Request request, Task task) {
+		Site site = null;
+		if (task != null) {
+			site = task.getSite();
+		}
+		Set<Integer> acceptStatCode;
+		String charset = null;
+		Map<String, String> headers = null;
+		if (site != null) {
+			acceptStatCode = site.getAcceptStatCode();
+			charset = site.getCharset();
+			headers = site.getHeaders();
+		} else {
+			acceptStatCode = Sets.newHashSet(200);
+		}
+		logger.info("downloading page {}", request.getUrl());
+		CloseableHttpResponse httpResponse = null;
+		int statusCode = 0;
+		try {
+			HttpHost proxyHost = null;
+			Proxy proxy = null; // TODO
+			if (site.getHttpProxyPool() != null
+					&& site.getHttpProxyPool().isEnable()) {
+				proxy = site.getHttpProxyFromPool();
+				proxyHost = proxy.getHttpHost();
+			} else if (site.getHttpProxy() != null) {
+				proxyHost = site.getHttpProxy();
+			}
 
-    @Override
-    public void setThread(int thread) {
-        httpClientGenerator.setPoolSize(thread);
-    }
+			HttpUriRequest httpUriRequest = getHttpUriRequest(request, site,
+					headers, proxyHost);// ���������˴���
+			httpResponse = getHttpClient(site, proxy).execute(httpUriRequest);// getHttpClient�������˴�����֤
+			statusCode = httpResponse.getStatusLine().getStatusCode();
+			request.putExtra(Request.STATUS_CODE, statusCode);
+			// 如果是正常的200状态码
+			if (statusAccept(acceptStatCode, statusCode)) {
+				Page page = handleResponse(request, charset, httpResponse, task);
+				onSuccess(request);
+				logger.info(request.getMethod() + " statusCode-->" + statusCode
+						+ " 获取页面成功");
+				return page;
+				// 如果是302
+			} else if (statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
+				logger.warn(request.getMethod() + " statusCode-->" + statusCode
+						+ " " + request.getUrl());
+				String locationUrl = httpResponse.getLastHeader("Location")
+						.getValue();
+				logger.info("获取重定向中的location---->" + locationUrl);
+				request = new Request(locationUrl);
+				Page page = download(request, task);
+				return page;
+				// 如果是401
+			} else if (statusCode == HttpStatus.SC_UNAUTHORIZED) {
+				logger.warn("未授权，禁止访问：" + statusCode + "\t" + request.getUrl());
+				return null;
+				//如果是403
+			} else if (statusCode==HttpStatus.SC_FORBIDDEN) {
+				logger.warn("Forbidden,没有权限访问该资源：" + statusCode + "\t" + request.getUrl());
+				return null;
+				//如果是404
+			} else if (statusCode==HttpStatus.SC_NOT_FOUND) {
+				logger.warn("该资源不存在，请检查访问路径：" + statusCode + "\t" + request.getUrl());
+				return null;
+			}else if (statusCode==HttpStatus.SC_INTERNAL_SERVER_ERROR) {
+				logger.warn("内部服务器错误：" + statusCode + "\t" + request.getUrl());
+				return null;
+			}else if (statusCode==HttpStatus.SC_SERVICE_UNAVAILABLE) {
+				logger.warn("服务无法获得：" + statusCode + "\t" + request.getUrl());
+				return null;
+			} else{
+				logger.warn("code error " + statusCode + "\t"
+						+ request.getUrl());
+				return null;
+			}
+		} catch (IOException e) {
+			logger.warn("download page " + request.getUrl() + " error", e);
+			if (site.getCycleRetryTimes() > 0) {
+				return addToCycleRetry(request, site);
+			}
+			onError(request);
+			return null;
+		} finally {
+			request.putExtra(Request.STATUS_CODE, statusCode);
+			if (site.getHttpProxyPool() != null
+					&& site.getHttpProxyPool().isEnable()) {
+				site.returnHttpProxyToPool(
+						(HttpHost) request.getExtra(Request.PROXY),
+						(Integer) request.getExtra(Request.STATUS_CODE));
+			}
+			try {
+				if (httpResponse != null) {
+					// ensure the connection is released back to pool
+					EntityUtils.consume(httpResponse.getEntity());
+				}
+			} catch (IOException e) {
+				logger.warn("close response fail", e);
+			}
+		}
+	}
 
-    protected boolean statusAccept(Set<Integer> acceptStatCode, int statusCode) {
-        return acceptStatCode.contains(statusCode);
-    }
+	@Override
+	public void setThread(int thread) {
+		httpClientGenerator.setPoolSize(thread);
+	}
 
-    protected HttpUriRequest getHttpUriRequest(Request request, Site site, Map<String, String> headers,HttpHost proxy) {
-        RequestBuilder requestBuilder = selectRequestMethod(request).setUri(request.getUrl());
-        if (headers != null) {
-            for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
-                requestBuilder.addHeader(headerEntry.getKey(), headerEntry.getValue());
-            }
-        }
-        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
-                .setConnectionRequestTimeout(site.getTimeOut())
-                .setSocketTimeout(site.getTimeOut())
-                .setConnectTimeout(site.getTimeOut())
-                .setCookieSpec(CookieSpecs.BEST_MATCH);
-        if (proxy !=null) {
+	protected boolean statusAccept(Set<Integer> acceptStatCode, int statusCode) {
+		return acceptStatCode.contains(statusCode);
+	}
+
+	protected HttpUriRequest getHttpUriRequest(Request request, Site site,
+			Map<String, String> headers, HttpHost proxy) {
+		RequestBuilder requestBuilder = selectRequestMethod(request).setUri(
+				request.getUrl());
+		if (headers != null) {
+			for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+				requestBuilder.addHeader(headerEntry.getKey(),
+						headerEntry.getValue());
+			}
+		}
+		RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
+				.setConnectionRequestTimeout(site.getTimeOut())
+				.setSocketTimeout(site.getTimeOut())
+				.setConnectTimeout(site.getTimeOut())
+				.setCookieSpec(CookieSpecs.BEST_MATCH);
+		if (proxy != null) {
 			requestConfigBuilder.setProxy(proxy);
 			request.putExtra(Request.PROXY, proxy);
 		}
-        requestBuilder.setConfig(requestConfigBuilder.build());
-        return requestBuilder.build();
-    }
+		requestBuilder.setConfig(requestConfigBuilder.build());
+		return requestBuilder.build();
+	}
 
-    protected RequestBuilder selectRequestMethod(Request request) {
-        String method = request.getMethod();
-        if (method == null || method.equalsIgnoreCase(HttpConstant.Method.GET)) {
-            //default get
-            return RequestBuilder.get();
-        } else if (method.equalsIgnoreCase(HttpConstant.Method.POST)) {
-            RequestBuilder requestBuilder = RequestBuilder.post();
-            NameValuePair[] nameValuePair = (NameValuePair[]) request.getExtra("nameValuePair");
-            if (nameValuePair != null && nameValuePair.length > 0) {
-                requestBuilder.addParameters(nameValuePair);
-            }
-            return requestBuilder;
-        } else if (method.equalsIgnoreCase(HttpConstant.Method.HEAD)) {
-            return RequestBuilder.head();
-        } else if (method.equalsIgnoreCase(HttpConstant.Method.PUT)) {
-            return RequestBuilder.put();
-        } else if (method.equalsIgnoreCase(HttpConstant.Method.DELETE)) {
-            return RequestBuilder.delete();
-        } else if (method.equalsIgnoreCase(HttpConstant.Method.TRACE)) {
-            return RequestBuilder.trace();
-        }
-        throw new IllegalArgumentException("Illegal HTTP Method " + method);
-    }
+	protected RequestBuilder selectRequestMethod(Request request) {
+		String method = request.getMethod();
+		if (method == null || method.equalsIgnoreCase(HttpConstant.Method.GET)) {
+			// default get
+			return RequestBuilder.get();
+		} else if (method.equalsIgnoreCase(HttpConstant.Method.POST)) {
+			RequestBuilder requestBuilder = RequestBuilder.post();
+			NameValuePair[] nameValuePair = (NameValuePair[]) request
+					.getExtra("nameValuePair");
+			if (nameValuePair != null && nameValuePair.length > 0) {
+				requestBuilder.addParameters(nameValuePair);
+			}
+			return requestBuilder;
+		} else if (method.equalsIgnoreCase(HttpConstant.Method.HEAD)) {
+			return RequestBuilder.head();
+		} else if (method.equalsIgnoreCase(HttpConstant.Method.PUT)) {
+			return RequestBuilder.put();
+		} else if (method.equalsIgnoreCase(HttpConstant.Method.DELETE)) {
+			return RequestBuilder.delete();
+		} else if (method.equalsIgnoreCase(HttpConstant.Method.TRACE)) {
+			return RequestBuilder.trace();
+		}
+		throw new IllegalArgumentException("Illegal HTTP Method " + method);
+	}
 
-    protected Page handleResponse(Request request, String charset, HttpResponse httpResponse, Task task) throws IOException {
-        String content = getContent(charset, httpResponse);
-        Page page = new Page();
-        page.setRawText(content);
-        page.setUrl(new PlainText(request.getUrl()));
-        page.setRequest(request);
-        page.setStatusCode(httpResponse.getStatusLine().getStatusCode());
-        return page;
-    }
+	protected Page handleResponse(Request request, String charset,
+			HttpResponse httpResponse, Task task) throws IOException {
+		String content = getContent(charset, httpResponse);
+		Page page = new Page();
+		page.setRawText(content);
+		page.setUrl(new PlainText(request.getUrl()));
+		page.setRequest(request);
+		page.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+		return page;
+	}
 
-    protected String getContent(String charset, HttpResponse httpResponse) throws IOException {
-        if (charset == null) {
-            byte[] contentBytes = IOUtils.toByteArray(httpResponse.getEntity().getContent());
-            String htmlCharset = getHtmlCharset(httpResponse, contentBytes);
-            if (htmlCharset != null) {
-                return new String(contentBytes, htmlCharset);
-            } else {
-                logger.warn("Charset autodetect failed, use {} as charset. Please specify charset in Site.setCharset()", Charset.defaultCharset());
-                return new String(contentBytes);
-            }
-        } else {
-            return IOUtils.toString(httpResponse.getEntity().getContent(), charset);
-        }
-    }
+	protected String getContent(String charset, HttpResponse httpResponse)
+			throws IOException {
+		if (charset == null) {
+			byte[] contentBytes = IOUtils.toByteArray(httpResponse.getEntity()
+					.getContent());
+			String htmlCharset = getHtmlCharset(httpResponse, contentBytes);
+			if (htmlCharset != null) {
+				return new String(contentBytes, htmlCharset);
+			} else {
+				logger.warn(
+						"Charset autodetect failed, use {} as charset. Please specify charset in Site.setCharset()",
+						Charset.defaultCharset());
+				return new String(contentBytes);
+			}
+		} else {
+			return IOUtils.toString(httpResponse.getEntity().getContent(),
+					charset);
+		}
+	}
 
-    protected String getHtmlCharset(HttpResponse httpResponse, byte[] contentBytes) throws IOException {
-        String charset;
-        // charset
-        // 1、encoding in http header Content-Type
-        String value = httpResponse.getEntity().getContentType().getValue();
-        charset = UrlUtils.getCharset(value);
-        if (StringUtils.isNotBlank(charset)) {
-            logger.debug("Auto get charset: {}", charset);
-            return charset;
-        }
-        // use default charset to decode first time
-        Charset defaultCharset = Charset.defaultCharset();
-        String content = new String(contentBytes, defaultCharset.name());
-        // 2、charset in meta
-        if (StringUtils.isNotEmpty(content)) {
-            Document document = Jsoup.parse(content);
-            Elements links = document.select("meta");
-            for (Element link : links) {
-                // 2.1、html4.01 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-                String metaContent = link.attr("content");
-                String metaCharset = link.attr("charset");
-                if (metaContent.indexOf("charset") != -1) {
-                    metaContent = metaContent.substring(metaContent.indexOf("charset"), metaContent.length());
-                    charset = metaContent.split("=")[1];
-                    break;
-                }
-                // 2.2、html5 <meta charset="UTF-8" />
-                else if (StringUtils.isNotEmpty(metaCharset)) {
-                    charset = metaCharset;
-                    break;
-                }
-            }
-        }
-        logger.debug("Auto get charset: {}", charset);
-        // 3、todo use tools as cpdetector for content decode
-        return charset;
-    }
+	protected String getHtmlCharset(HttpResponse httpResponse,
+			byte[] contentBytes) throws IOException {
+		String charset;
+		// charset
+		// 1、encoding in http header Content-Type
+		String value = httpResponse.getEntity().getContentType().getValue();
+		charset = UrlUtils.getCharset(value);
+		if (StringUtils.isNotBlank(charset)) {
+			logger.debug("Auto get charset: {}", charset);
+			return charset;
+		}
+		// use default charset to decode first time
+		Charset defaultCharset = Charset.defaultCharset();
+		String content = new String(contentBytes, defaultCharset.name());
+		// 2、charset in meta
+		if (StringUtils.isNotEmpty(content)) {
+			Document document = Jsoup.parse(content);
+			Elements links = document.select("meta");
+			for (Element link : links) {
+				// 2.1、html4.01 <meta http-equiv="Content-Type"
+				// content="text/html; charset=UTF-8" />
+				String metaContent = link.attr("content");
+				String metaCharset = link.attr("charset");
+				if (metaContent.indexOf("charset") != -1) {
+					metaContent = metaContent.substring(
+							metaContent.indexOf("charset"),
+							metaContent.length());
+					charset = metaContent.split("=")[1];
+					break;
+				}
+				// 2.2、html5 <meta charset="UTF-8" />
+				else if (StringUtils.isNotEmpty(metaCharset)) {
+					charset = metaCharset;
+					break;
+				}
+			}
+		}
+		logger.debug("Auto get charset: {}", charset);
+		// 3、todo use tools as cpdetector for content decode
+		return charset;
+	}
 }
